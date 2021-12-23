771e39470d8612f0520946beff0c36e7e9856450
Sara-Barbi <barbisara02@gmail.com>
2021-12-23 14:21:12 +0100

    funziona

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 let go = document.getElementById("start");
-
+let bombs = [];
 go.addEventListener ("click", function(){
     let selezione = document.getElementById("myselect").value;
     let block = document.getElementById("block");  //creo una variabile per non riscrivere sempre document.getElem eccc..
@@ -22,88 +22,90 @@ go.addEventListener ("click", function(){
         }
     }   
     
-    let num = document.getElementsByClassName("num");       //richiamo l'elemento che esiste solo dentro alla funzione, dentro alla funzione.
-    for(i=0; i<num.length; i++){                            //parte da 0 e abilita tutti i num perche faccio num.lenght
-        num[i].addEventListener("click", cellaClick);       //inserisco una click,function a ciascun num, quindi scrivo num[i] (Ascoltatore di ogni elemento).
-       }                                                    //richiamo la funzione che ho ceato dopo per ciascun elemento.
     
-    const posizioniList = generaBombe();
     
-    //potevo scrivere direttamente la function all'interno del for                                                  
     
-    function cellaClick(){ 
-       const valoreCella = parseInt(this.textContent);     //creo una variabile che mi restituisce il valore dell'elem. cliccato              
-       this.classList.add("change");                       //aggiungo a quell'elem la classe change
-       console.log(valoreCella);
-    }
+    
+
+    
+
+    
+
+    
+    
+    
+    
+    
  
   
 
     //16 bombe in modo casuale con un math random. per non far uscire lo stesso num, ogni volta che estraggo una posiz, la metto in un array. e utilizzare l'if pe
+   
     
-
-    function generaBombe(){
-        let bombe = [];
-        let bombeTotali = 16;
-        //tentativiPossibili = num.length - bombeTotali;
-
-
-        //generiamo le bombe
-       while(bombe.lenght < num.length ){
-           let newPosition = generaRandomInt (1, bombeTotali);
-           if(bombe.includes(newPosition)){
-               bombe.push(newPosition)
-               console.log(bombe[i]);
-           }else 
-
-           
-           
-           console.log(bombe);
-       }
-       return bombe; 
-    }
- /*     
-    function generaElementoArrayDopp(arrayValori, valore){
-        for(let i=0; i<bombe.lenght; i++){
-            if(arrayValori[i]==valore){
-                return true
-            }
-        }
-        return false
-    }
-    function generaRandomInt(min, max) {
-        let result = Math.floor(Math.random * (min - max +1))+min;
-        return result
-    }
     
-    function fineGioco(){
-        const listaCelle = document.querySelectorAll("num");
-        for(let i= 0; i< listaCelle.length; i++){
-            const cella = listaCelle[i]
-            const valorerCella = parseInt(this.querySelector("div").textContent); 
-            if(generaElementoArrayDopp(bombe ,valoreCella) {
-                cella.classList.add("changered")
-            }
-        }
-        console.log("FINE");
-    }
+    
+    let num = document.getElementsByClassName("num");       //richiamo l'elemento che esiste solo dentro alla funzione, dentro alla funzione.
+    for(i=0; i<num.length; i++){                            //parte da 0 e abilita tutti i num perche faccio num.lenght
+        num[i].addEventListener("click", cellaClick);       //inserisco una click,function a ciascun num, quindi scrivo num[i] (Ascoltatore di ogni elemento).
+    }                                                    //richiamo la funzione che ho ceato dopo per ciascun elemento.
+    
+    
+    //potevo scrivere direttamente la function all'interno del for                                                  
     
     function cellaClick(){ 
-        const valorerCella = parseInt(this.querySelector("div").textContent);  
-        if(generaElementoArrayDopp(bombe,valoreCella)){
-            this.classList.add("changered")
-            fineGioco();
-        }else{
-            this.classList.add("change")
-        }                   
+        const valoreCella = parseInt(this.textContent);     //creo una variabile che mi restituisce il valore dell'elem. cliccato              
+        this.classList.add("change");                       //aggiungo a quell'elem la classe change
         console.log(valoreCella);
     }
+
+
+
+    const item = document.querySelectorAll(".num");
+    functionBombs();
     
-    */
-    }
-    
-    
-    
-    
-    
-);
+
+
+    function functionBombs() {  
+        for (let i = 0; i < 16; i++) {
+            let randomBombs = Math.floor(Math.random()*item.length +1);
+            let truetest = bombs.includes(randomBombs);
+                if(truetest==false){
+                    bombs.push(randomBombs);
+                
+                    
+                }else{
+                    i--;
+                }
+        }
+        console.log(bombs)
+       
+        for(let i = 0; i < item.length; i++) {
+
+                item[i].addEventListener("click", function() {
+                
+                    let numtest= parseInt(this.innerHTML);
+                
+                    if (bombs.includes(numtest)==true) {
+                    this.style.backgroundColor = "#DC143C";
+                   
+                    cellView();
+               
+                }else{
+                    this.style.backgroundColor = "#6495ED";
+                }
+            });
+       
+            function cellView(){
+                for(let i = 0; i < item.length; i++) {
+                    
+                    if (bombs.includes(parseInt(item[i].innerHTML))==true) {
+                        item[i].style.backgroundColor = "#DC143C";
+                    }else{
+                        item[i].style.backgroundColor = "#6495ED";
+                    }
+                    
+                }
+                
+            }                                        
+    }    
+    }});
